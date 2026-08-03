89d61c8ae47069016c89a0a815c73eaf73f2fa8d
Matheus C Marques <matheuscamarques@gmail.com>
2026-08-03 13:19:15 -0300

    config(harness): aponta para OTP 30 local (/opt/erlang/30-stock e 30-pon)

--- harness/config/baseline.sh
+++ harness/config/baseline.sh
@@ -3,6 +3,6 @@
 # ATENÇÃO: Build OTP 30 leva ~30 min. Enquanto isso, usamos o Erlang 29 do sistema.
 # Para resultados reais, execute: make build-stock && make build-pon
 
-OTP_BASELINE_ROOT=${OTP_BASELINE_ROOT:-/home/sanonichan/.asdf/installs/erlang/29.0.3}
+OTP_BASELINE_ROOT=${OTP_BASELINE_ROOT:-/opt/erlang/30-stock}
 BASELINE_ERL=${OTP_BASELINE_ROOT}/bin/erl
 BASELINE_ERLC=${OTP_BASELINE_ROOT}/bin/erlc
--- harness/config/ponbeam.sh
+++ harness/config/ponbeam.sh
@@ -3,6 +3,6 @@
 # ATENÇÃO: quando o build PON-BEAM estiver pronto, mude para:
 #   OTP_PONBEAM_ROOT=/opt/erlang/30-pon
 
-OTP_PONBEAM_ROOT=${OTP_PONBEAM_ROOT:-/home/sanonichan/.asdf/installs/erlang/29.0.3}
+OTP_PONBEAM_ROOT=${OTP_PONBEAM_ROOT:-/opt/erlang/30-pon}
 PONBEAM_ERL=${OTP_PONBEAM_ROOT}/bin/erl
 PONBEAM_ERLC=${OTP_PONBEAM_ROOT}/bin/erlc
